e259465a95f73feacb7609347e61153f29507acf
Shift <shift@laravelshift.com>
2024-01-30 05:05:31 +0000

    Rename ENV variables

--- public/js/app.js
+++ public/js/app.js
@@ -34485,8 +34485,8 @@ if (token) {
 // window.Pusher = require('pusher-js');
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
 //     encrypted: true
 // });
 
